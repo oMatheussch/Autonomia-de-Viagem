ba5b14316b80fb9e3184e759ec0a685a447762e6
Matheus Schmidt <matisschmidt@gmail.com>
2023-10-17 22:21:03 -0300

    Melhorando retorno do cácluclo realicado

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function printTravel() {
     var lit = document.getElementById("liters").value;
     var valorLitro = document.getElementById("value").value;
     var autonomia = (dist/lit).toFixed(2);
-    document.getElementById("result").innerHTML = "Em sua viagem para " + viagem + " com a distância de " + dist + " quilometros foram gastos " + lit +" litros de combustível! Ao final da viagem você teve uma autonomia de " + autonomia + "Km/L, o valor gasto por KM foi de R$" + (valorLitro/autonomia).toFixed(2);
+    window.alert("Em sua viagem para " + viagem + " com a distância de " + dist + " quilometros foram gastos " + lit +" litros de combustível! Ao final da viagem você teve uma autonomia de " + autonomia + "Km/L, o valor gasto por KM foi de R$" + (valorLitro/autonomia).toFixed(2));
 }
